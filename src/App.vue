11323e4812688a7ef280b4bd0b0d1c2f80c32b0c
maricegalla <maricegalla@gmail.com>
2022-09-15 13:34:37 -0300

    feat: add page hero

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <page-hero/>
   </div>
 </template>
--- src/components/PageHero.vue
+++ src/components/PageHero.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-    
+  <div class="d-flex align-items-center justify-content-md-center mx-8 my-6">
+    <div>
+      <h1 class="font-weight-bold">Biomas Brasileiros</h1>
+      <p>Bioma é um conjunto de vida vegetal e animal, constituído pelo agrupamento de tipos de vegetação que são próximos e que podem ser identificados em nível regional, com condições de geologia e clima semelhantes e que, historicamente, sofreram os mesmos processos de formação da paisagem, resultando em uma diversidade de flora e fauna própria.</p>
+    </div>
+    <img alt="Map" src="../assets/map.png" height="300">
   </div>
 </template>
 
