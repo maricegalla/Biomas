4e823e4196b17194f3c34bde7b10fd57a5d9be69
maricegalla <maricegalla@gmail.com>
2022-09-15 14:26:59 -0300

    feat: list and map styled

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <page-hero/>
+    <page-list/>
   </div>
 </template>
 
 <script>
-import PageHero from './components/PageHero.vue'
+import PageHero from './components/PageHero.vue';
+import PageList from './components/PageList.vue';
 
 export default {
   name: 'App',
   components: {
     PageHero,
+    PageList,
   }
 }
 </script>
---
+++ src/components/PageList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container my-5">
+    <div class="row justify-content-center align-items-center">
+      <div class="col-auto">
+        <h2 class="font-weight-bold text-primary">Biomas existentes</h2>
+        <ul class=" text-primary">
+          <li>Amazônia</li>
+          <li>Caatinga</li>
+          <li>Cerrado</li>
+          <li>Mata Atlântica</li>
+          <li>Pampa</li>
+          <li>Pantanal</li>
+        </ul>
+      </div>
+      <div class="col-auto">
+        <img alt="Biomas" src="../assets/biomas.png" height="500">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageList',
+}
+</script>
