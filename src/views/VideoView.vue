b8343e819d090fa1fa81cbcf74ab8f9f885453f8
maricegalla <maricegalla@gmail.com>
2022-09-15 21:24:24 -0300

    feat: added embeded videos

--- src/views/VideoView.vue
+++ src/views/VideoView.vue
@@ -9,10 +9,12 @@
         </div>
       </div>
     </div>
-    <div>
+    <div v-for="(video, index) in videos" :key="index">
       <p align="center" class="mt-5">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/eEPabXAVzNA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="560" height="315" :src="video" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </p>
+    </div>
+    <div class="mb-5">
       <router-link to="/">
           <button type="button" class="rounded-pill mx-auto d-block btn btn-primary" >
             <b-icon-arrow-return-left />
@@ -26,5 +28,18 @@
 <script>
 export default {
   name: 'VideoView',
+  data() {
+    return {
+      videos: [
+        'https://www.youtube.com/embed/eEPabXAVzNA',
+        'https://www.youtube.com/embed/vg9-DLSuo7Q',
+        'https://www.youtube.com/embed/IIO6agFJMOw',
+        'https://www.youtube.com/embed/GnS4SlV8hmY',
+        'https://www.youtube.com/embed/yxRolBxeDEU',
+        'https://www.youtube.com/embed/9iw71uHPYtQ',
+        'https://www.youtube.com/embed/TBEDhBCO1ow',
+      ]
+    }
+  }
 }
 </script>
